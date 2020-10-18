cf53724585f81aa252f1219432b3795a677a71fa
Dell Hsu <delldell90114@gmail.com>
2020-10-18 11:04:01 +0800

    Add <Person> model and add a scaffold view of <List> type

--- 20201018_MVC5_CLASS_01/Controllers/TestController.cs
+++ 20201018_MVC5_CLASS_01/Controllers/TestController.cs
@@ -14,11 +14,11 @@ namespace _20201018_MVC5_CLASS_01.Controllers
         {
             var data = new List<Person>
             {
-                new Person{Id=1, Name="Dell", Age=20 },
-                new Person{Id=2, Name="Aiyu", Age=21 },
-                new Person{Id=3, Name="Bunch", Age=22 },
-                new Person{Id=4, Name="Egenda", Age=23 },
-                new Person{Id=5, Name="Test", Age=24 }
+                new Person(){Id=1, Name="Dell", Age=20 },
+                new Person(){Id=2, Name="Aiyu", Age=21 },
+                new Person(){Id=3, Name="Bunch", Age=22 },
+                new Person(){Id=4, Name="Egenda", Age=23 },
+                new Person(){Id=5, Name="Test", Age=24 }
             };
 
             return View(data);
