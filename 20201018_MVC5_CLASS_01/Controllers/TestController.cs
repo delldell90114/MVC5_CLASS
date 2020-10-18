ee103bd8655a1007f9dfa75cf5df76e974b2bb89
Dell Hsu <delldell90114@gmail.com>
2020-10-18 12:00:19 +0800

    Add <create> function for person class

--- 20201018_MVC5_CLASS_01/Controllers/TestController.cs
+++ 20201018_MVC5_CLASS_01/Controllers/TestController.cs
@@ -9,19 +9,36 @@ namespace _20201018_MVC5_CLASS_01.Controllers
 {
     public class TestController : Controller
     {
+        static List<Person>data = new List<Person>
+        {
+            new Person(){Id=1, Name="Dell", Age=20 },
+            new Person(){Id=2, Name="Aiyu", Age=21 },
+            new Person(){Id=3, Name="Bunch", Age=22 },
+            new Person(){Id=4, Name="Egenda", Age=23 },
+            new Person(){Id=5, Name="Test", Age=24 }
+        };
+
         // GET: Test
         public ActionResult Index()
-        {
-            var data = new List<Person>
-            {
-                new Person(){Id=1, Name="Dell", Age=20 },
-                new Person(){Id=2, Name="Aiyu", Age=21 },
-                new Person(){Id=3, Name="Bunch", Age=22 },
-                new Person(){Id=4, Name="Egenda", Age=23 },
-                new Person(){Id=5, Name="Test", Age=24 }
-            };
-
+        {   
             return View(data);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                data.Add(person);
+                return RedirectToAction("Index");
+            }
+
+            return View(person);
         }
     }
 }
