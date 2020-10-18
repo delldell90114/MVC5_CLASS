fd0e0a7a0d3aa6b16f599efda366d9cec968e300
Dell Hsu <delldell90114@gmail.com>
2020-10-18 14:30:01 +0800

    Add <Edit/Delete/Details>

--- 20201018_MVC5_CLASS_01/Controllers/TestController.cs
+++ 20201018_MVC5_CLASS_01/Controllers/TestController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Web;
 using System.Web.Mvc;
 using _20201018_MVC5_CLASS_01.Models;
@@ -39,6 +40,55 @@ namespace _20201018_MVC5_CLASS_01.Controllers
             }
 
             return View(person);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View(data.FirstOrDefault(p => p.Id == id));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                /*
+                data.FirstOrDefault(p => p.Id == id).Name = person.Name;
+                data.FirstOrDefault(p => p.Id == id).Age = person.Age;
+                */
+
+                var newData = data.FirstOrDefault(p => p.Id == id);
+                newData.Name= person.Name;
+                newData.Age = person.Age;
+
+                return RedirectToAction("Index");
+            }
+
+            return View(id);
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View(data.FirstOrDefault(p => p.Id == id));
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View(data.FirstOrDefault(p => p.Id == id));
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection form)
+        {
+            data.Remove(data.First(p => p.Id == id));
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id, int a)
+        {
+            data.Remove(data.First(p => p.Id == id));
+            return RedirectToAction("Index");
         }
     }
 }
