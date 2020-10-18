dd265a56c575b8df4c8341a88cd724ea85fedf09
Dell Hsu <delldell90114@gmail.com>
2020-10-18 15:07:47 +0800

    加入validation(包含前後端的驗證, 前端使用jQuery套件, 而後端驗證功能已被.NET包起來實作)

--- 20201018_MVC5_CLASS_01/Models/Person.cs
+++ 20201018_MVC5_CLASS_01/Models/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,10 @@ namespace _20201018_MVC5_CLASS_01.Models
     public class Person
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "請輸入姓名:")]
         public String Name { get; set; }
+        [Required(ErrorMessage ="請輸入年紀:")]
+        [Range(5,99, ErrorMessage = "請輸入範圍:5~99")]
         public int Age { get; set; }
     }
 }
