cb464ba6e66a2bc2f0fb40df8f02155d670d2327
Dell Hsu <delldell90114@gmail.com>
2020-10-24 13:55:00 +0800

    Prevent <Over Post> attack: 1. Use ViewModel without model directly (If there has some field was not binded when CRUD) 2. Use <ValueImjecter> for fields binding.

--- 20201018_MVC5_CLASS_01/Models/DepartmentController.cs
+++ 20201018_MVC5_CLASS_01/Models/DepartmentController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using WebGrease;
+using Omu.ValueInjecter;
+using _20201018_MVC5_CLASS_01.ViewModel;
 
 namespace _20201018_MVC5_CLASS_01.Models
 {
@@ -39,42 +42,56 @@ namespace _20201018_MVC5_CLASS_01.Models
         }
         public ActionResult Edit(int? id)
         {
-            if(!id.HasValue)
+            if (id == null)
             {
-                return this.HttpNotFound();
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
 
             var dept = db.Department.Find(id);
-
+            if(dept == null)
+            {
+                return this.HttpNotFound();
+            }
             ViewBag.InstructorID = new SelectList(db.Person, "ID", "FirstName", dept.InstructorID);
             return View(db.Department.Find(id));
         }
 
+        // ** Use ViewModel can prevent the <Over post> attack **
         [HttpPost]
-        public ActionResult Edit(int id, Department data)
+        //public ActionResult Edit(int id, Department data)
+        public ActionResult Edit(int id, DepartmentEditVewModel data)
         {
             if (ModelState.IsValid)
             {
                 var item = db.Department.Find(id);
-                item.Name = data.Name;
-                item.Budget = data.Budget;
-                item.StartDate = data.StartDate;
-                item.InstructorID = data.InstructorID;
+                // -> ** Use <ValueInjecter> for binding each field, the name of property must be same will be binded **
+                item.InjectFrom(data);
+                
+                // -> ** Following binded can be marked after usiung <ValueInjecter>
+                //item.Name = data.Name;
+                //item.Budget = data.Budget;
+                //item.StartDate = data.StartDate;
+                //item.InstructorID = data.InstructorID;
 
                 db.SaveChanges();
 
                 return RedirectToAction("Index");
             }
 
-            ViewBag.InstructorID = new SelectList(db.Person, "ID", "FirstName");
+            var dept = db.Department.Find(id);
+            if (dept == null)
+            {
+                return this.HttpNotFound();
+            }
+            ViewBag.InstructorID = new SelectList(db.Person, "ID", "FirstName", dept.InstructorID);
             return View(db.Department.Find(id));
         }
 
         public ActionResult Details(int? id)
         {
-            if(!id.HasValue)
+            if (id == null)
             {
-                return this.HttpNotFound();
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
 
             return View(db.Department.Find(id));
@@ -82,9 +99,9 @@ namespace _20201018_MVC5_CLASS_01.Models
 
         public ActionResult Delete(int? id)
         {
-            if(!id.HasValue)
+            if (id == null)
             {
-                return this.HttpNotFound();
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
 
             ViewBag.InstructorID = new SelectList(db.Person, "ID", "FirstName");
---
+++ 20201018_MVC5_CLASS_01/ViewModel/DepartmentEditVewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace _20201018_MVC5_CLASS_01.ViewModel
+{
+    public class DepartmentEditVewModel
+    {
+        public int DepartmentID { get; set; }
+        public string Name { get; set; }
+        [DisplayFormat(DataFormatString = "{0:0000}")]
+        public decimal Budget { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public System.DateTime StartDate { get; set; }
+        public Nullable<int> InstructorID { get; set; }
+    }
+}
