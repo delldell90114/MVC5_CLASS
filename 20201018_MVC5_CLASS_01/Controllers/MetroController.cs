731af491f12de073b465d2f51fa98eaa29c142a1
Dell Hsu <delldell90114@gmail.com>
2020-11-01 13:05:46 +0800

    完成 Bootstrap_Metro_Dashboard 主版面套用

---
+++ 20201018_MVC5_CLASS_01/Controllers/MetroController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _20201018_MVC5_CLASS_01.Controllers
+{
+    public class MetroController : Controller
+    {
+        // GET: Metro
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
